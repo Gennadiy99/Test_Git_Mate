ddb66aa3ee528a67579e5e5110a43b44920df82b
Gennadiy99 <login841@gmail.com>
2024-11-02 21:34:58 +0100

    function call options

--- main.js
+++ main.js
@@ -16,6 +16,15 @@ function calculateGuests(guestsInput) {
     return 'not a number';
   }
   countGuests = parseInt(countGuests, 10);
-  return countGuests;
+  return `Количество гостей: ${countGuests}`;
 }
 console.log(calculateGuests('we would like to book for 3 people'));
+console.log(calculateGuests('I think 5 guests'));
+console.log(calculateGuests('Big company of 15 dudes'));
+console.log(calculateGuests('5'));
+console.log(calculateGuests('alone'));
+console.log(calculateGuests('0'));
+console.log(calculateGuests('Hello, 9.75 people'));
+console.log(calculateGuests('There will be 7 or 9 guys'));
+console.log(calculateGuests('hello cat walks on my keyboard ksadjfhskaj12.34kasdfhsjk'));
+
